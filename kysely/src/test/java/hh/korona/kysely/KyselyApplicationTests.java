e5c3c93b83db8f021a78dc8a3d081725dc12ea83
joni miettinen <joni9909@hotmail.fi>
2020-04-02 09:55:43 +0300

    first commit

---
+++ kysely/src/test/java/hh/korona/kysely/KyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.korona.kysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
