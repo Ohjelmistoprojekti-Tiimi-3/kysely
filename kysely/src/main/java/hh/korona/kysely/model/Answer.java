4c0cf5c4488b122ef572a39c672cdaf09335d327
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-04-07 14:44:18 +0300

    Added Answer.java

--- kysely/src/main/java/hh/korona/kysely/model/Answer.java
+++ kysely/src/main/java/hh/korona/kysely/model/Answer.java
@@ -2,6 +2,7 @@ package hh.korona.kysely.model;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Answer {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
+    @Column(name = "answer_id")
     private Long id;
 	
 	@Column(name = "answer", nullable = false)
@@ -24,9 +25,58 @@ public class Answer {
     @Column(name = "date", nullable = false)
     private Date date;
     
-	@ManyToOne
-	@JsonIgnore
-	  @JoinColumn(name = "question_id")
-	    private User user;
+    @ManyToOne(cascade = CascadeType.ALL)
+	    private Question question;
+
+	public Answer() {
+		super();
+	}
+
+	public Answer(Long id, String answer, Date date, Question question) {
+		super();
+		this.id = id;
+		this.answer = answer;
+		this.date = date;
+		this.question = question;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", answer=" + answer + ", date=" + date + "]";
+	}
+    
+    
 
 }
