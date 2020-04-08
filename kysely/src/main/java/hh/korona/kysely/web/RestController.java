b2619bb516822e0dfb1ab92e23c2a7072adc9f09
HotSpor <hotspor00@gmail.com>
2020-04-08 16:42:48 +0300

    Rest controller and methods added

--- kysely/src/main/java/hh/korona/kysely/web/RestController.java
+++ kysely/src/main/java/hh/korona/kysely/web/RestController.java
@@ -1,8 +1,38 @@
 package hh.korona.kysely.web;
 
 
+import hh.korona.kysely.model.Query;
+import hh.korona.kysely.model.Question;
+import hh.korona.kysely.repository.QueryRepository;
+import hh.korona.kysely.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @Autowired
+    QueryRepository queryRepository;
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+
+    @RequestMapping(value = "/api/kyselyt", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Query> returnRestQueryList() {
+        return (List<Query>) queryRepository.findAll();
+    }
+
+    @RequestMapping(value = "/api/kysymykset", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Question> returnRestQuestionList() {
+        return (List<Question>) questionRepository.findAll();
+    }
 
 
 }
